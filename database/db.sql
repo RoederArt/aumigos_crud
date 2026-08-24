3e03e53cdad32b5c7448e6993157356e1752e0b7
RoederArt <arthur_roeder@estudante.sesisenai.org.br>
2026-08-24 11:05:51 -0300

    feat - tabela de clientes criada

--- database/db.sql
+++ database/db.sql
@@ -2,6 +2,12 @@ CREATE DATABASE cadastro_aumigos;
 
 use cadastro_aumigos;
 
+CREATE TABLE clientes(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome_cliente VARCHAR(100) NOT NULL,
+    email_cliente VARCHAR(100) NOT NULL,
+    numero_clientes INT NOT NULL
+);
 
 
 
