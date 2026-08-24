9f701ddfe9678d77fe94be1a59a77be57e41b3e5
RoederArt <arthur_roeder@estudante.sesisenai.org.br>
2026-08-24 11:07:12 -0300

    feat - tabela de animais criada

--- database/db.sql
+++ database/db.sql
@@ -9,6 +9,14 @@ CREATE TABLE clientes(
     numero_clientes INT NOT NULL
 );
 
+CREATE TABLE animais(
+    id_animais INT PRIMARY KEY AUTO_INCREMENT,
+    nome_animal VARCHAR(100) NOT NULL,
+    raca_animal VARCHAR(100) NOT NULL,
+    idade_animal INT NOT NULL
+    id_clientes INT NOT NULL,
+    FOREIGN KEY (id_clientes) REFERENCES clientes(id)
+);
 
 
 
