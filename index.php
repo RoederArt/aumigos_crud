c66e3e48b547929f2d763aa3c789e46473cd0ec3
RoederArt <arthur_roeder@estudante.sesisenai.org.br>
2026-08-26 12:55:46 -0300

    feat - adicionando lista ao index

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+
+
+<?php
+
+/*include '/..infra/connect.php';
+$sql = "SELECT * FROM animais";
+$result = mysqli_query($conexao, $sql);
+*/
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,11 +22,64 @@
 <header>
 <nav>
 <a href="public/cadastro_clientes.php">CADASTRO DE CLIENTES</a>
+<br>
 <a href="public/cadastro_animais.php">CADASTRO DE ANIMAIS</a>
 </nav>
 </header>
 
-<main></main>
+<main>
+
+<h1>Tabela de animais cadastrados</h1>
+        <form method="POST">
+            <label for="usuario">Filtro por Usuário</label>
+            <select name="cliente" id="">
+                <option value="">Todos</option>
+
+            <?php
+            $sql_clientes = "SELECT * FROM clientes";
+            $result_clientes = mysqli_query($conexao, $sql_clientes);
+            while ($clientes = mysqli_fetch_assoc($result_clientes)) {
+                echo "<option value ='{$usuario['id']}'>{$usuario['nome']}</option>";
+            }
+            ?>
+            </select>
+
+            <button type="submit">Filtrar</button>
+            <br>
+            <br>
+            <form>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>nome</th>
+                            <th>email</th>
+                            <th>numero</th>
+                            <th>ID do cliente</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <?php
+                        
+                            while ($animal = mysqli_fetch_assoc($result_clientes)) {
+
+                            echo "<tr>";
+                            echo "<td>{$animal['nome']}</td>";
+                            echo "<td>{$animal['raça']}</td>";
+                            echo "<td>{$animal['idade']}</td>";
+                            echo "<td>{$animal['dono_do_animal']}</td>";
+                            echo "";
+
+
+                            }
+                        
+                            ?>                        
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+
+</main>
 
 <footer></footer>
     
